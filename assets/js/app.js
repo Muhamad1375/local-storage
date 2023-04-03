05eb0f66cba697cb6945a27d8482bd5e901ede10
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-04-04 02:32:31 +0330

    first step

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,16 @@
 
 
 //Event Listeners
-
+eventListener();
 function eventListener() {
       //Form submission
-      document.querySelector('#form').addEventListener('submit')
+      document.querySelector('#form').addEventListener('submit', newTweet);
 }
 
 
 
 //Functions
+function newTweet(e) {
+      e.preventDefault();
+      console.log('Form Submitted');
+}
